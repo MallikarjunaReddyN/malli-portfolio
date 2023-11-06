1f3c89a3d64845e41c99080154e85e1a31fdeaa9
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2023-11-06 12:51:04 +0530

    fixed azure logo size issue

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,7 +17,7 @@ import mysql from "../assets/images/mysql-logo.png"
 import postgresql from "../assets/images/postgresql-logo.svg"
 import redis from "../assets/images/redis-logo.svg"
 import github from "../assets/images/github-logo.png"
-import azure from "../assets/images/azure.jpeg"
+import azure from "../assets/images/azure.png"
 
 const Skills = () => {
   const skills = [
